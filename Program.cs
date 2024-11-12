016860dfc8d99dc19c15bb8fda62f20ed43d31e6
semyoniuditskii <Seweer21@gmail.com>
2024-11-12 17:35:32 +0500

    New Version

--- Program.cs
+++ Program.cs
@@ -30,19 +30,40 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-app.MapGet("/constructions/{id:int}",ConstructionStore.ConstructionDB.GetConstruction);
-app.MapGet("/constructions", ConstructionStore.ConstructionDB.GetConstructions);
-app.MapPost("/constructions", ConstructionStore.ConstructionDB.CreateConstruction);
-app.MapPut("/constructions", ConstructionStore.ConstructionDB.UpdateConstruction);
-app.MapDelete("/constructions{id:int}", ConstructionStore.ConstructionDB.RemoveCostruction);
+// app.MapGet("/constructions/{id:int}",ConstructionStore.ConstructionDB.GetConstruction);
+// app.MapGet("/constructions", ConstructionStore.ConstructionDB.GetConstructions);
+// app.MapPost("/constructions", ConstructionStore.ConstructionDB.CreateConstruction);
+// app.MapPut("/constructions", ConstructionStore.ConstructionDB.UpdateConstruction);
+// app.MapDelete("/constructions{id:int}", ConstructionStore.ConstructionDB.RemoveCostruction);
 
-// app.MapGet("/constructions", async (ConstructionDb db) => await db.Constructions.ToListAsync());
-// app.MapPost("/construction", async (ConstructionDb db, Construction construction) =>
-// {
-//     await db.Constructions.AddAsync(construction);
-//     await db.SaveChangesAsync();
-//     return Results.Created($"/construction/{construction.Id}", construction);
-// });
+app.MapGet("/constructions", async (ConstructionDb db) => await db.Constructions.ToListAsync());
+app.MapPost("/construction", async (ConstructionDb db, Construction construction) =>
+{
+    await db.Constructions.AddAsync(construction);
+    await db.SaveChangesAsync();
+    return Results.Created($"/construction/{construction.Id}", construction);
+});
+app.MapGet("/construction/{id}", async (ConstructionDb db, int id) => await db.Constructions.FindAsync(id));
+app.MapPut("/construction/{id}", async (ConstructionDb db, Construction updateConstruction, int id) =>
+{
+    var construction = await db.Constructions.FindAsync(id);
+    if (construction is null){ return Results.NotFound();}
+    construction.Name = updateConstruction.Name;
+    construction.Description = updateConstruction.Description;
+    await db.SaveChangesAsync();
+    return Results.NoContent();
+});
+app.MapDelete("/construction/{id}", async (ConstructionDb db, int id) =>
+{
+    var construction = await db.Constructions.FindAsync(id);
+    if (construction is null)
+    {
+        return Results.NotFound();
+    }
 
+    db.Constructions.Remove(construction);
+    await db.SaveChangesAsync();
+    return Results.Ok();
+});
 
 app.Run();
--- obj/Debug/net8.0/ConstructionStore.AssemblyInfo.cs
+++ obj/Debug/net8.0/ConstructionStore.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConstructionStore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fbb64a0b888c38793bcde012f51714eaacaa706d")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConstructionStore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConstructionStore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
